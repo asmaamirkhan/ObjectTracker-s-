9ae1778ed0c1dd6d8d457b87314a4e4eca314380
Asmaa Mirkhan <45575570+asmaamirkhan@users.noreply.github.com>
2019-09-15 13:14:29 +0300

    Output error handling 🕵️‍♀️

--- track_me.py
+++ track_me.py
@@ -6,6 +6,7 @@ import cv2 as cv
 import time
 import os.path
 
+
 def choose_tracker(tracker):
     OPENCV_OBJECT_TRACKERS = {
         "csrt": cv.TrackerCSRT_create,
@@ -27,13 +28,15 @@ def choose_tracker(tracker):
     return wanted_tracker, tracker
 
 
-def track(wanted_tracker, video_file, tracker, save):
+def track(wanted_tracker, args):
     tracking_win = "Object Tracking"
     cropped_win = "Tracked Region"
-    cap = cv.VideoCapture(video_file)
-    if save:
-        fourcc = cv.VideoWriter_fourcc(*'MP4V')
-        output = cv.VideoWriter('output.mp4', fourcc, 20.0, (700,700))
+    cap = cv.VideoCapture(args.video_file)
+    if args.output_path:
+        fourcc = cv.VideoWriter_fourcc(*'mp4v')
+        print(args.output_path + '.mp4')
+        output = cv.VideoWriter(
+            args.output_path + '.mp4', fourcc, 20.0, (700, 700))
     frame_counter = 0
     init_box = None
 
@@ -56,7 +59,6 @@ def track(wanted_tracker, video_file, tracker, save):
                                     showCrosshair=True)
             wanted_tracker.init(frame, init_box)
 
-        
         if init_box is not None:
             success, box = wanted_tracker.update(frame)
 
@@ -68,33 +70,33 @@ def track(wanted_tracker, video_file, tracker, save):
                 cv.imshow(cropped_win, crop)
             else:
                 cv.destroyWindow(cropped_win)
-        end = time.clock()
-        print('Frame: {} , Elapsed time: {:.3f}'.format(frame_counter, end-start))
-        print('=========================================')
-        cv.putText(frame, 'Tracker: {}, Frame: {}'.format(tracker, frame_counter), (30, 30),
+
+        cv.putText(frame, 'Tracker: {}, Frame: {}'.format(args.tracker, frame_counter), (30, 30),
                    cv.FONT_HERSHEY_COMPLEX, 0.5, (255, 0, 255), 1)
         cv.imshow(tracking_win, frame)
-        if save:
+        end = time.clock()
+        #print('Frame: {} , Elapsed time: {:.3f}'.format(frame_counter, end-start))
+        # print('=========================================')
+        if args.output_path:
             output.write(frame)
 
 
 def main(args):
     wanted_tracker, args.tracker = choose_tracker(args.tracker)
-    track(wanted_tracker, args.video_file, args.tracker, save = args.save)
+    track(wanted_tracker, args)
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Object tracking parameters')
     parser.add_argument('-v', '--video_file',
-                        help='Path to your video file', type=str)
+                        help='Path to your video file', type=str, required=True)
     parser.add_argument('-t', '--tracker',
                         help='Object tracking algorithm', type=str, default='kcf')
-    parser.add_argument(
-        '-s', '--save', help='Save output video', type=bool, default=False)
-    #parser.add_argument('','--output_path')
+    parser.add_argument('-o', '--output_path',
+                        help='Output file path', type=str)
     args = parser.parse_args()
-    print(args)
-
     assert os.path.isfile(args.video_file), 'Invalid input file'
+    assert os.path.isdir(os.path.dirname(
+        args.output_path)), 'No such directory'
 
     main(args)
