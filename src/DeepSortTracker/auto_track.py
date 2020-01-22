5d0f07524cb4a4f151a68dffc84386f5786b996b
Asmaa Mirkhan <asmaamirkhan.am@gmail.com>
2020-01-22 20:40:51 +0300

    Formatting 🚧

--- src/DeepSortTracker/auto_track.py
+++ src/DeepSortTracker/auto_track.py
@@ -19,32 +19,50 @@ METRIC = nn_matching.NearestNeighborDistanceMetric("cosine",
                                                    NN_BUDGET)
 
 
+def adapt_to_deep_sort(boxes):
+    for box in boxes:
+        box = list(box)
+        box[0], box[1] = box[1], box[0]
+        box[2], box[3] = box[3], box[2]
+    return boxes
+
 def main(args):
+    # setting deep sort parameters
     tracker = Tracker(METRIC)
     encoder = gdet.create_box_encoder(args.tracker, batch_size=1)
+
+    # Initialize the object detector
     detector = DetectorAPI(args.model_path)
+
+    # open the video
     cap = cv.VideoCapture(args.video_file)
 
+    # create output file
     if args.output_path:
         fourcc = cv.VideoWriter_fourcc(*'MP4V')
+        # cap.get(3) = widht, cap.get(4) height
         output = cv.VideoWriter(args.output_path, fourcc, 20.0,
                                 (int(cap.get(3)), int(cap.get(4))))
 
+    # read the video frame by frame
     while True:
         check, frame = cap.read()
+
+        # initialize tracking parameters
         sort_tracking_params = []
 
+        # do real detection
+        # boxes are in (x_top_left, y_top_left, x_bottom_right, y_bottom_right) format
         boxes, scores, classes, number = detector.processFrame(frame,
                                                                debug_time=True)
+        
+        # filter boxes due to threshold
         boxes = [
             boxes[i] for i in range(0, number) if scores[i] > args.threshold
         ]
 
-        for box in boxes:
-            box = list(box)
-            box[0], box[1] = box[1], box[0]
-            box[2], box[3] = box[3], box[2]
-
+        # do tracking
+        boxes = adapt_to_deep_sort(boxes)
         features = encoder(frame, boxes)
         detections = [
             Detection(tbox, 1.0, feature)
@@ -53,6 +71,7 @@ def main(args):
         tracker.predict()
         tracker.update(detections)
 
+        # draw boxes due to tracking results
         for track in tracker.tracks:
             if not track.is_confirmed() or track.time_since_update > 1:
                 continue
@@ -69,6 +88,7 @@ def main(args):
 
         cv.imshow(WINDOW_TITLE, frame)
 
+        # save frames to specified path
         if args.output_path:
             output.write(frame)
 
