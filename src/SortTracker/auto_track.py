c957a573560fc91d417bac4d8f5f34778fcbeb11
Asmaa Mirkhan <asmaamirkhan.am@gmail.com>
2020-01-22 19:54:35 +0300

    Adding class names to objects 👩‍🏫

--- src/SortTracker/auto_track.py
+++ src/SortTracker/auto_track.py
@@ -1,10 +1,15 @@
 import os
 from sort.sort import *
 from detection_utils.DetectorAPI import DetectorAPI
+from detection_utils.ms_coco_classnames import MS_CLASSES
 import cv2 as cv
 import argparse
 
 WINDOW_TITLE = 'Tracker'
+
+
+def clean_result(result, number):
+    return result[:number]
 
 
 def main(args):
@@ -18,25 +23,32 @@ def main(args):
         boxes, scores, classes, number = detector.processFrame(frame,
                                                                debug_time=True)
 
+        boxes = clean_result(boxes, number)
+        scores = clean_result(scores, number)
+        classes = clean_result(classes, number)
+
         for i, box in enumerate(boxes):
             sort_tracking_params.append(
                 [box[0], box[1], box[2], box[3], scores[i]])
-        
+
         sort_tracking_params = np.array(sort_tracking_params)
         trackers = tracker.update(sort_tracking_params).astype(np.int32)
-        
+
         for index, box in enumerate(trackers):
             if scores[index] > args.threshold:
                 cv.rectangle(frame, (box[1], box[0]), (box[3], box[2]),
                              (255, 255, 255),
                              thickness=2)
                 cv.putText(frame,
-                           'id: {}'.format(box[4]), (box[1], box[0]),
-                           cv.FONT_HERSHEY_PLAIN, 1,(255, 255, 255),
+                           'class: {}, id: {}'.format(
+                               MS_CLASSES[classes[index]], box[4]),
+                           (box[1], box[0]),
+                           cv.FONT_HERSHEY_PLAIN,
+                           1, (255, 255, 255),
                            thickness=2)
 
         cv.imshow(WINDOW_TITLE, frame)
-        
+
         # the end of the video?
         if not check:
             break
